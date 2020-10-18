4b3c550f904b320bd2343d1b5fe3582f2dae8114
Mohamadhassan Ebrahimi <emohamadhassan@gmail.com>
2020-10-18 15:05:51 +0330

    Some improvements on SDL

---
+++ src/api/SDL.ts
@@ -0,0 +1,56 @@
+type Primitives = "string" | "number" | "boolean";
+
+type ArrayItemType = {
+  type: Primitives;
+  nullable?: true;
+};
+
+type ArrayType = [ArrayItemType];
+
+export type DefType = {
+  type: Primitives | ArrayType;
+  nullable?: true;
+  maybe?: true;
+};
+
+type PrimitiveInferType<T extends Primitives> = T extends "string" ? string : T extends "number" ? number : boolean;
+
+type Nullable<T> = T | null;
+
+type Maybe<T> = T | undefined;
+
+type InferPrimitiveOrArray<T extends DefType> = T["type"] extends Primitives
+  ? PrimitiveInferType<T["type"]>
+  : T["type"] extends ArrayType
+  ? Array<InferNullable<T["type"][0]>>
+  : never;
+
+type InferNullable<T extends DefType | ArrayItemType> = T["nullable"] extends true
+  ? Nullable<InferPrimitiveOrArray<T>>
+  : InferPrimitiveOrArray<T>;
+
+type InferMaybeNullable<T extends DefType> = T["maybe"] extends true
+  ? T["nullable"] extends true
+    ? Nullable<Maybe<InferPrimitiveOrArray<T>>>
+    : Maybe<InferPrimitiveOrArray<T>>
+  : InferNullable<T>;
+
+type SchemaModel = {[k: string]: DefType};
+
+type SchemaInferType<T extends DefType> = InferMaybeNullable<T>;
+
+type SchemaResultType<T extends SchemaModel> = {[k in keyof T]: SchemaInferType<T[k]>};
+
+function Inferable<T extends SchemaModel>(model: T) {
+  return {} as SchemaResultType<T>;
+}
+
+const newComment = Inferable({
+  title: {type: "string", maybe: true, nullable: true},
+  id: {type: "number"},
+  approved: {type: "boolean", maybe: true},
+  parent: {type: "boolean", nullable: true, maybe: true},
+  children: {type: [{type: "string", nullable: true}], maybe: true},
+});
+
+const titolo = newComment.children;
--- src/q.ts
+++ src/q.ts
@@ -1,27 +1,96 @@
+// import {SchemaType} from "./types";
+//
+// type TVariableType = {[k in keyof typeof commentType]: SchemaInferType<typeof commentType[k]>};
+// interface IQuery {
+//   /**
+//    * params going to be like {
+//    *  search : string,
+//    *  orderBy : ['DES' , 'ASC']
+//    * }
+//    *
+//    */
+//   params?: Record<string, SchemaType | Readonly<Array<string /*| SchemaType*/>>>;
+//   /**
+//    *  {
+//    *      blogId : 'number'
+//    *  }
+//    */
+//   variables?: Record<string, SchemaType>;
+//   //url type must be change to /string type
+//   url: string;
+// }
+//
+// const apis = {
+//   getAll: {
+//     url: "/posts",
+//     params: {
+//       search: "string?",
+//       orderBy: ["DES", "ASC"],
+//     },
+//     variables: {
+//       blogId: "number",
+//     },
+//   } as const,
+// } as const;
+//
+// function withMyQuery(queryKey: keyof typeof apis) {
+//   const query = apis[queryKey];
+//   return query.variables;
+// }
+//
+// const a = withMyQuery("getAll");
+//
+// export function MakeConst<T extends Readonly<IQuery[]>>(type: T) {
+//   const newType = type.map((t) => ({...t} as const));
+//   return newType;
+// }
+// // const createApi = (apiArr: IQuery[]) => {
+// // const constApi = MakeConst(apis);
+// // };
+// interface NonType {
+//   name: string;
+// }
+//
+// function makeConstAgain<T extends ReadonlyArray<NonType>>(obj: T) {
+//   return obj;
+// }
+
 import {SchemaType} from "./types";
 
-// type TVariableType = {[k in keyof typeof commentType]: SchemaInferType<typeof commentType[k]>};
 interface IQuery {
-  type: string;
+  readonly type: string;
   /**
    * params going to be like {
    *  search : string,
-   *  orderBy : ['DES' , 'ASC']
+   *  orderBy : [‘DES’ , ‘ASC’]
    * }
    *
    */
-  params?: Record<string, SchemaType | Readonly<Array<string /*| SchemaType*/>>>;
+  readonly params?: Record<string, SchemaType | Readonly<Array<string /*| SchemaType*/>>>;
   /**
    *  {
-   *      blogId : 'number'
+   *      blogId : ‘number’
    *  }
    */
-  variables?: Record<string, SchemaType>;
+  readonly variables?: Record<string, SchemaType>;
   //url type must be change to /string type
-  url: string;
+  readonly url: string;
 }
 
-const apis: Readonly<IQuery[]> = [
+// type QueryType = {
+//   [k: string]: IQuery;
+// };
+// const felan =
+export function makeApiSchema<T extends Readonly<IQuery[]>>(type: T) {
+  return type;
+}
+
+function makeConstArray<T extends ReadonlyArray<IQuery>>(t: T) {
+  const constantObjs = t.map((value) => ({...value} as const));
+  return [...constantObjs] as const;
+}
+
+const a = makeConstArray([
   {
     type: "GET_ALL",
     url: "/posts",
@@ -32,14 +101,47 @@ const apis: Readonly<IQuery[]> = [
     variables: {
       blogId: "number",
     },
-  } as const,
-];
-const felan = apis[0];
+  },
+  {
+    type: "GET_SOME",
+    url: "/post",
+    params: {
+      filter: "string?",
+    },
+    variables: {
+      postId: "number",
+    },
+  },
+]);
 
-export function MakeConst<T extends Readonly<IQuery[]>>(type: T) {
-  const newType = type.map((t) => ({...t} as const));
-  return newType;
-}
-// const createApi = (apiArr: IQuery[]) => {
-const constApi = MakeConst(apis);
-// };
+const myApis = makeApiSchema([
+  {
+    type: "GET_ALL",
+    url: "/posts",
+    params: {
+      search: "string?",
+      orderBy: ["DES", "ASC"],
+    },
+    variables: {
+      blogId: "number",
+    },
+  },
+  {
+    type: "GET_SOME",
+    url: "/post",
+    params: {
+      filter: "string?",
+    },
+    variables: {
+      postId: "number",
+    },
+  },
+] as const);
+const [, {type: fofo}] = myApis;
+
+const myApis2 = makeApiSchema(a);
+
+const inf = myApis2[0].type;
+
+console.log(fofo);
+/**my Apis has currect type */
