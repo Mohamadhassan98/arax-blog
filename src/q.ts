bddc87fcad6d22a10c43c636bf2341c284a283af
alisajadih <alisajadih@gmail.com>
2020-10-18 17:31:57 +0330

    working on schema

--- src/q.ts
+++ src/q.ts
@@ -3,6 +3,7 @@ import {SchemaType} from "./types";
 // type TVariableType = {[k in keyof typeof commentType]: SchemaInferType<typeof commentType[k]>};
 interface IQuery {
   type: string;
+
   /**
    * params going to be like {
    *  search : string,
@@ -20,8 +21,18 @@ interface IQuery {
   //url type must be change to /string type
   url: string;
 }
+// type QueryType = {
+//   [k: string]: IQuery;
+// };
 
-const apis: Readonly<IQuery[]> = [
+
+// const felan =
+
+export function makeApiSchema<T extends Readonly<IQuery[]>>(type: T) {
+  return type;
+}
+
+const myApis = makeApiSchema([
   {
     type: "GET_ALL",
     url: "/posts",
@@ -32,14 +43,46 @@ const apis: Readonly<IQuery[]> = [
     variables: {
       blogId: "number",
     },
-  } as const,
-];
-const felan = apis[0];
+  },
+] as const);
+/**my Apis has currect type */
 
-export function MakeConst<T extends Readonly<IQuery[]>>(type: T) {
-  const newType = type.map((t) => ({...t} as const));
-  return newType;
-}
+
+const apis /*:QueryType*/ = {
+    GET_ALL: {
+      url: "/posts",
+      params: {
+        search: "string?",
+        orderBy: ["DES", "ASC"],
+      },
+      variables: {
+        blogId: "number",
+      },
+    },
+  } as const;
+  
 // const createApi = (apiArr: IQuery[]) => {
-const constApi = MakeConst(apis);
+// const constApi = MakeConst(apis);
 // };
+
+interface tt {
+  name: string;
+}
+function forTsOnly<T extends Readonly<tt[]>>(x: T) {
+  return x;
+}
+const newYY = forTsOnly([{name: "ali"}, {name: "mohammad"}, {name: "feal"}] as const);
+
+const w = newYY[0].name;
+
+const y = [
+  {
+    name: "ali",
+  },
+  {
+    name: "mohammad",
+  },
+] as const;
+const newY = forTsOnly(({} as const) as Readonly<tt[]>);
+
+// const w: (keyof typeof y[0])[0] = y[0];
